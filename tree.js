dce459371a74d355583013aeb31bac5e513dcca5
quentinikeno <ikenoq@gmail.com>
2022-09-11 17:41:02 -1000

    Add method for countEvens.

--- tree.js
+++ tree.js
@@ -29,7 +29,18 @@ class Tree {
 
 	/** countEvens(): count all of the nodes in the tree with even values. */
 
-	countEvens() {}
+	countEvens(count = 0) {
+		if (this.root) {
+			let toVisitStack = [this.root];
+
+			while (toVisitStack.length) {
+				let current = toVisitStack.pop();
+				if (current.val % 2 === 0) count += 1;
+				for (let child of current.children) toVisitStack.push(child);
+			}
+		}
+		return count;
+	}
 
 	/** numGreater(lowerBound): return a count of the number of nodes
 	 * whose value is greater than lowerBound. */
